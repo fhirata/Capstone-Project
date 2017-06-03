5bbcb99000dbdcd68f4fd1df81cbcc5baece3f68
Fabio Hirata <fabio.hirata@gmail.com>
2017-06-03 15:29:14 -0700

    Temporary work around for click bug on detail view.

--- CupertinoJudo/app/src/main/java/com/cupertinojudo/android/UnitsFormatterUtil.java
+++ CupertinoJudo/app/src/main/java/com/cupertinojudo/android/UnitsFormatterUtil.java
@@ -2,13 +2,15 @@ package com.cupertinojudo.android;
 
 import android.content.Context;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  *
  */
 
 public class UnitsFormatterUtil {
     public static String formatWeight(Context context, int weight) {
-        String weightInPounds = context.getString(R.string.weight_in_pounds);
+        String weightInPounds = checkNotNull(context).getString(R.string.weight_in_pounds);
         return String.format(weightInPounds, weight);
     }
 }
